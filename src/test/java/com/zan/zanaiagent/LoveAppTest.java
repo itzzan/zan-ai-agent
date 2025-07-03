56e9b215afa5e5d2bbabf88250966a3512d6908f
zhangziyan <zhangziyan@cnstrong.cn>
2025-07-03 15:17:56 +0800

    feat(AI智能体): 简单的AI调用

---
+++ src/test/java/com/zan/zanaiagent/LoveAppTest.java
@@ -0,0 +1,39 @@
+package com.zan.zanaiagent;
+
+import cn.hutool.core.lang.UUID;
+import com.zan.zanaiagent.app.LoveApp;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author Zan
+ * @Create 2025/7/3 15:14
+ * @ClassName: LoveAppTest
+ * @Description : TODO 请用一句话描述该类的功能 
+ */
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员Zan";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮
+        message = "我想让另一半（编程导航）更爱我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
+
